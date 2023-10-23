219540bd83bbd5dce5eebf1a79deaf1f651da42a
Rio Edwards <rioredwards@gmail.com>
2023-10-23 16:43:27 -0700

    Touchup styling in hero sections

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,18 +9,18 @@ interface HeroProps {
 
 const Hero: React.FC<HeroProps> = ({ PrimaryText, SecondaryText, AvatarURL }) => {
   return (
-    <section className="w-full flex justify-between items-center flex-col">
-      <div className="mt-10 mb-4">
+    <section className="w-full flex justify-around items-center flex-col">
+      <div>
         <ContentfulImage
           alt="Rio Edwards"
-          className="object-contain h-full rounded-full"
+          className="object-contain h-full rounded-full mt-20"
           height={384}
           width={260}
           src={AvatarURL}
         />
       </div>
-      <span className="text-4xl mt-4 mb-4">{PrimaryText}</span>
-      <span className="text-2xl mt-2 mb-10">{SecondaryText}</span>
+      <span className="text-4xl mt-12">{PrimaryText}</span>
+      <span className="text-2xl mt-8 mb-28">{SecondaryText}</span>
     </section>
   );
 };
