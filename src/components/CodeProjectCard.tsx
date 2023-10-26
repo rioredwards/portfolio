c05c9401bcec38b721c654a258010bb3a401d225
Rio Edwards <rioredwards@gmail.com>
2023-10-26 11:11:30 -0700

    Preview pngs for gifs loading and displaying when not hover

--- src/components/CodeProjectCard.tsx
+++ src/components/CodeProjectCard.tsx
@@ -1,4 +1,5 @@
 import ContentfulImage from '@/lib/contentful-image';
+import { base64StrPngFromGif } from 'gif-png-converter';
 import Link from 'next/link';
 
 interface NewCodeProjectCardProps {
@@ -10,7 +11,9 @@ interface NewCodeProjectCardProps {
   tags: string[];
 }
 
-const NewCodeProjectCard: React.FC<NewCodeProjectCardProps> = ({ title, preview, tags }) => {
+const NewCodeProjectCard: React.FC<NewCodeProjectCardProps> = async ({ title, preview, tags }) => {
+  const previewPng = await base64StrPngFromGif(preview.url);
+
   return (
     <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
       <article className="group relative flex flex-col items-center h-[260px] overflow-hidden rounded-lg hover:shadow-lg">
@@ -19,11 +22,18 @@ const NewCodeProjectCard: React.FC<NewCodeProjectCardProps> = ({ title, preview,
             width={400}
             height={260}
             alt={preview.title}
-            className="inset-0 object-cover w-full h-full"
+            className="block group-hover:hidden inset-0 object-cover w-full h-full"
+            src={previewPng}
+          />
+          <ContentfulImage
+            width={400}
+            height={260}
+            alt={preview.title}
+            className="hidden group-hover:block z-10 inset-0 object-cover w-full h-full"
             src={preview.url}
           />
         </Link>
-        <header className="hidden group-hover:flex items-center justify-between p-2 md:p-4 z-10 absolute top-1/2">
+        <header className="flex group-hover:hidden items-center justify-between p-2 md:p-4 z-10 absolute top-1/2">
           <h1 className="text-lg">
             <a className="no-underline text-white text-2xl font-bold" href="#">
               {title}
