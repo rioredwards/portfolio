cf31212000c312ecf654f64a727da255c7c23778
Rio Edwards <rioredwards@gmail.com>
2023-10-31 00:16:19 -0700

    create seperate WebsiteCard component

--- src/components/CodeProjectCard.tsx
+++ src/components/CodeProjectCard.tsx
@@ -1,10 +1,19 @@
 'use client';
 import Link from 'next/link';
 import { CodeProject } from '@/lib/api';
-import CodeCardContainer from './CodeCardContainer';
 import { useState } from 'react';
 import CodeCardImage from './CodeCardImage';
 import { adjustColor } from '@/utils/colorUtils';
+import WebsiteCard from './WebsiteCard';
+
+const MAX_TITLE_LENGTH = 28;
+
+function limitTitle(title: string) {
+  if (title.length > MAX_TITLE_LENGTH) {
+    return title.slice(0, MAX_TITLE_LENGTH) + '...';
+  }
+  return title;
+}
 
 const generateBgGradient = (bgColor: string): string => {
   const topRightColor = adjustColor(bgColor, 10, -20);
@@ -17,6 +26,7 @@ const generateBgGradient = (bgColor: string): string => {
 const CodeProjectCard: React.FC<CodeProject> = ({ title, codeCardIcon }) => {
   const [isHover, setIsHover] = useState(false);
   const bgGradient = isHover ? generateBgGradient(codeCardIcon?.bgColor) : undefined;
+  const titleLimited = limitTitle(title);
   const type = codeCardIcon?.type || 'website';
 
   return (
@@ -26,12 +36,19 @@ const CodeProjectCard: React.FC<CodeProject> = ({ title, codeCardIcon }) => {
       }}
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
-      className={`my-1 w-full group bg-slate-50 relative flex flex-col items-center h-[260px] overflow-hidden rounded-4xl hover:shadow-lg cursor-pointer`}
+      className={`my-1 w-full group bg-gray-100 relative flex flex-col items-center h-[260px] overflow-hidden rounded-4xl hover:shadow-lg cursor-pointer`}
     >
       <Link href={`/${title}`} className="w-full h-full flex flex-col items-center justify-start">
-        <CodeCardContainer type={type} title={title}>
-          {codeCardIcon && <CodeCardImage key={title} isHover={isHover} {...codeCardIcon} />}
-        </CodeCardContainer>
+        {type === 'website' && (
+          <WebsiteCard title={titleLimited}>
+            {codeCardIcon && <CodeCardImage key={title} isHover={isHover} {...codeCardIcon} />}
+          </WebsiteCard>
+        )}
+        {type === 'cli' && (
+          <WebsiteCard title={titleLimited}>
+            {codeCardIcon && <CodeCardImage key={title} isHover={isHover} {...codeCardIcon} />}
+          </WebsiteCard>
+        )}
       </Link>
     </article>
   );
--- src/components/WebsiteCard.tsx
+++ src/components/WebsiteCard.tsx
@@ -1,31 +1,19 @@
-import { CodeCardType } from '@/lib/api';
-
 interface Props {
-  type: CodeCardType;
   title: string;
-  children?: React.ReactNode;
+  children: React.ReactNode;
 }
 
-const MAX_TITLE_LENGTH = 28;
-
-function limitTitle(title: string) {
-  if (title.length > MAX_TITLE_LENGTH) {
-    return title.slice(0, MAX_TITLE_LENGTH) + '...';
-  }
-  return title;
-}
-
-const CodeCardContainer: React.FC<Props> = ({ title, children }) => {
+const WebsiteCard: React.FC<Props> = ({ title, children }) => {
   return (
     <>
-      <div className="w-full h-16 bg-slate-100 flex items-center px-8 gap-3 group-hover:border-b-2">
+      <div className="w-full h-16 bg-gray-100 flex items-center px-8 gap-3 group-hover:border-b-2">
         <div className="h-full w-28 flex items-center justify-between">
           <div className="h-8 w-8 rounded-full group-hover:h-7 group-hover:w-7 bg-white group-hover:bg-gradient-to-br group-hover:from-red-200 group-hover:to-red-400" />
           <div className="h-8 w-8 rounded-full group-hover:h-7 group-hover:w-7 bg-white group-hover:bg-gradient-to-br group-hover:from-yellow-200 group-hover:to-yellow-400" />
           <div className="h-8 w-8 rounded-full group-hover:h-7 group-hover:w-7 bg-white group-hover:bg-gradient-to-br group-hover:from-green-200 group-hover:to-green-400" />
         </div>
         <div className="flex items-center grow bg-white h-8 rounded-md px-2">
-          <h3 className="text-xl font-bold text-gray-500 whitespace-nowrap">{limitTitle(title)}</h3>
+          <h3 className="text-xl font-bold text-gray-500 whitespace-nowrap">{title}</h3>
         </div>
       </div>
       {children}
@@ -33,4 +21,4 @@ const CodeCardContainer: React.FC<Props> = ({ title, children }) => {
   );
 };
 
-export default CodeCardContainer;
+export default WebsiteCard;
