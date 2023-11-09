c9d4f25e70a139229acd30fbd3a7d2d51c4424dd
Rio Edwards <rioredwards@gmail.com>
2023-11-09 11:20:43 -0800

    Refactor Mobile Nav Links into dedicated component

--- app/ui/header/MobileNavModal.tsx
+++ app/ui/header/MobileNavModal.tsx
@@ -1,15 +1,48 @@
 'use client';
 import { useRef } from 'react';
 import { Dialog } from '@headlessui/react';
-import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { MobileNavLink } from './MobileNavLink';
 
 interface Props {
   isOpen: boolean;
   setIsOpen: Function;
 }
 
+const links = [
+  {
+    text: 'HOME',
+    url: '/',
+    color: '#FF008C',
+  },
+  {
+    text: 'CODE',
+    url: '/',
+    color: '#D309E1',
+  },
+  {
+    text: 'ART',
+    url: '/',
+    color: '#9C1AFF',
+  },
+  {
+    text: 'ABOUT',
+    url: '/',
+    color: '#7700FF',
+  },
+  {
+    text: 'CONTACT',
+    url: '/',
+    color: '#4400FF',
+  },
+];
+
 const MobileNavModal: React.FC<Props> = ({ isOpen, setIsOpen }) => {
   const closeButtonRef = useRef(null);
+
+  const hideMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
     <Dialog
@@ -17,7 +50,7 @@ const MobileNavModal: React.FC<Props> = ({ isOpen, setIsOpen }) => {
       className="z-10"
       initialFocus={closeButtonRef}
       open={isOpen}
-      onClose={() => setIsOpen(false)}
+      onClose={hideMenu}
     >
       <Dialog.Panel className="fixed inset-0 w-screen h-screen overflow-hidden bg-white text-center">
         {/* Close Button */}
@@ -26,7 +59,7 @@ const MobileNavModal: React.FC<Props> = ({ isOpen, setIsOpen }) => {
             type="button"
             className="h-12 w-12 rounded-full"
             ref={closeButtonRef}
-            onClick={() => setIsOpen(false)}
+            onClick={hideMenu}
           />
         </div>
         <div className="w-full mt-12 px-8 py-8">
@@ -35,35 +68,18 @@ const MobileNavModal: React.FC<Props> = ({ isOpen, setIsOpen }) => {
           </Dialog.Title>
           <p className="mt-2 text-lg text-gray-500">Developer / Designer / Creator</p>
           {/* Links */}
-          <section className="mt-12 w-full flex flex-col items-center gap-4">
-            <Link
-              className="block w-full py-4 whitespace-nowrap flex-shrink-0 bg-slate-200 rounded-full"
-              href="/"
-              onClick={() => setIsOpen(false)}
-            >
-              <span className="text-lg leading-loose">HOME</span>
-            </Link>
-            <Link
-              className="block w-full py-4 whitespace-nowrap flex-shrink-0 bg-slate-200 rounded-full"
-              href="/"
-              onClick={() => setIsOpen(false)}
-            >
-              <span className="text-lg leading-loose">CODE</span>
-            </Link>
-            <Link
-              className="block w-full py-4 whitespace-nowrap flex-shrink-0 bg-slate-200 rounded-full"
-              href="/"
-              onClick={() => setIsOpen(false)}
-            >
-              <span className="text-lg leading-loose">ART</span>
-            </Link>
-            <Link
-              className="block w-full py-4 whitespace-nowrap flex-shrink-0 bg-slate-200 rounded-full"
-              href="/"
-              onClick={() => setIsOpen(false)}
-            >
-              <span className="text-lg leading-loose">CONTACT</span>
-            </Link>
+          <section className="mt-12 w-full">
+            <motion.ul className="w-full flex flex-col items-center gap-4">
+              {links.map((link, idx) => (
+                <MobileNavLink
+                  key={idx}
+                  text={link.text}
+                  url={link.url}
+                  color={link.color}
+                  hideMenu={hideMenu}
+                />
+              ))}
+            </motion.ul>
           </section>
         </div>
       </Dialog.Panel>
