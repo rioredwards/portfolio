3911f9f1a8df8d823495aea16ffeaaeecb4f4c92
Rio Edwards <rioredwards@gmail.com>
2024-01-26 13:40:08 -0800

    Add subtext to art cards

--- app/ui/art/ArtCardTitle.tsx
+++ app/ui/art/ArtCardTitle.tsx
@@ -10,20 +10,31 @@ interface Props {
 const spring = {
   type: 'spring',
   damping: 20,
-  stiffness: 100,
+  stiffness: 60,
 };
 
-const ArtCardTitleContainerVariants: Variants = {
+const containerVariants: Variants = {
   isExpanded: {
-    y: -146,
-    height: 56,
-    paddingBottom: 16,
+    y: -238,
+    height: 150,
+    paddingBottom: 10,
     transition: spring,
   },
   isNotExpanded: {
     y: -156,
     height: 130,
-    paddingBottom: 60,
+    paddingBottom: 20,
+    transition: spring,
+  },
+};
+
+const subTextVariants: Variants = {
+  isExpanded: {
+    opacity: 1,
+    transition: spring,
+  },
+  isNotExpanded: {
+    opacity: 0,
     transition: spring,
   },
 };
@@ -33,13 +44,26 @@ const ArtCardTitle: React.FC<Props> = ({ isExpanded, title, width }) => {
     <motion.div
       initial={false}
       animate={isExpanded ? 'isExpanded' : 'isNotExpanded'}
-      variants={ArtCardTitleContainerVariants}
+      variants={containerVariants}
       className={clsx(
         width,
-        'absolute z-10 flex items-end justify-center bg-gradient-to-b from-transparent to-black/75'
+        { 'items-start': isExpanded },
+        { 'items-center': !isExpanded },
+        'absolute z-10 flex flex-col justify-end bg-gradient-to-b from-transparent to-black/75'
       )}
     >
-      <motion.h3 className="text-3xl font-black text-white">{title}</motion.h3>
+      <motion.h3 layout className="mx-4 text-3xl font-black text-white">
+        {title}
+      </motion.h3>
+      <motion.span
+        animate={isExpanded ? 'isExpanded' : 'isNotExpanded'}
+        variants={subTextVariants}
+        initial={false}
+        layout
+        className="mx-4 text-md font-black text-white"
+      >
+        Decipherment shores of the cosmic ocean take root
+      </motion.span>
     </motion.div>
   );
 };
