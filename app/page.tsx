dff5f1435dfedaeb886754b9777a9c7b217ebc65
Rio Edwards <rioredwards@gmail.com>
2025-11-15 15:58:04 -0800

    feat: reset to default next app

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,65 @@
-import { draftMode } from 'next/headers';
-import { getHeroContent } from '@/lib/api';
-import Hero from '@/ui/hero/Hero';
-import CodeCardsSection from './ui/code/CodeCardsSection';
-import ContactForm from './ui/contact/ContactForm';
-import About from './ui/about/About';
-import Art from './ui/art/Art';
+import Image from "next/image";
 
-export default async function Page() {
-  const { isEnabled: draftModeIsEnabled } = draftMode();
-  const heroContent = await getHeroContent(draftModeIsEnabled);
-
+export default function Home() {
   return (
-    <div className="w-full flex flex-col items-center justify-start">
-      <section className="mt-44 2xl:mt-72 container px-5 flex justify-around items-center flex-col">
-        <Hero {...heroContent} />
-      </section>
-      <hr />
-      <div className="mt-24 2xl:mt-36 mb-24 w-full flex items-center justify-center">
-        <section className="w-full py-6 px-0" id="code">
-          <CodeCardsSection />
-        </section>
-      </div>
-      <section className="w-full py-6 px-0 mb-28 flex flex-col items-center justify-start" id="art">
-        <Art />
-      </section>
-      <section
-        className="w-full py-6 px-0 mb-28 flex flex-col items-center justify-start"
-        id="about"
-      >
-        <About />
-      </section>
-      <section className="w-full py-6 px-0" id="contact">
-        <ContactForm />
-      </section>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Head over to{" "}
+            <a
+              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Templates
+            </a>{" "}
+            or the{" "}
+            <a
+              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+              className="font-medium text-zinc-950 dark:text-zinc-50"
+            >
+              Learning
+            </a>{" "}
+            center.
+          </p>
+        </div>
+        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={16}
+              height={16}
+            />
+            Deploy Now
+          </a>
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </main>
     </div>
   );
 }
