cb2931f61c1a33502dc806000fb4648bf33d5a98
Rio Edwards <rioredwards@gmail.com>
2025-12-26 20:10:51 -0800

    continue merge

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <main className="bg-background min-h-screen">
       <section id="home">
-        <div className="max-content-max-width mx-auto px-4 pt-24 pb-56 md:px-20 md:pt-48 md:pb-64">
+        <div className="max-w-content-max-w px-content-px md:px-content-px-md py-content-py mx-auto grid min-h-screen place-items-center">
           <Hero
             title="Hello, I'm Rio."
             paragraphs={[
